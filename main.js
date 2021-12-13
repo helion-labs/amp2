b4dfa6626faa87b2a368828a69c969402523267d
SamH <sam@terahelion.ca>
2021-12-12 21:40:38 -0800

    adding form

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ import { Auth } from 'aws-amplify';
 function myFunction() {
     alert("The form was submitted");
 }
+
+const form = document.getElementById('form');
+form.addEventListener('submit', myFunction);
